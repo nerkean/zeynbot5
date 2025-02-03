184e296626d7c42fb167565bf7ac8232252118a7
nerkean <147550511+nerkean@users.noreply.github.com>
2025-02-03 11:01:00 +0200

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 10000;
 const LEADERBOARD_CACHE_TTL = 5 * 60;
 
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://bandazeyna.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200,
